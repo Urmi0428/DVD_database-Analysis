aa43c0bcaf954fec3c4a5a96ab36260f1975f7e4
Urmila Jadeja <urmi.jadeja04@gmail.com>
2023-06-28 17:17:58 -0400

    removed unwanted files

--- queries.sql
+++ queries.sql
@@ -1,36 +1,53 @@
-SELECT a.actor_id,CONCAT(a.first_name,' ', a.last_name) as Actor,fi.title as Film,COUNT(CONCAT(c.first_name,' ', c.last_name)) as No_of_Customer, COUNT(a.actor_id) as No_of_Actors_In_Film  FROM actor a
-JOIN film_actor f ON f.actor_id = a.actor_id
-JOIN film fi ON fi.film_id = f.film_id
-JOIN inventory i ON i.film_id  = f.film_id 
-JOIN rental r ON r.inventory_id = i.inventory_id
-JOIN customer c ON c.customer_id = r.customer_id
-GROUP BY a.actor_id, c.first_name,c.last_name,fi.title
-HAVING c.first_name LIKE '%r%' AND c.last_name LIKE '%h%' AND COUNT(a.actor_id) >1
-ORDER BY CONCAT(a.first_name,' ', a.last_name);
+/*first query- Since most people watch comdey now a days with many actors List Comdey films that have more than two actors*/
+SELECT f.title as Comedy_Film, COUNT(a.actor_id)as No_of_actor FROM film f
+join film_actor fa ON fa.film_id = f.film_id
+Join actor a on a.actor_id = fa.actor_id
+join film_category fc ON fc.film_id = f.film_id
+Join category c ON c.category_id = fc.category_id
+Group by f.title,c.name
+Having c.name ='Comedy' AND COUNT(a.actor_id)>2
 
-SELECT * 
+/*second query- customer name Linda called for issue in her payment so pull up the data of her past rentals*/
+SELECT c.first_name, c.last_name, ci.city, co.country, p.amount,p.Payment_date
 FROM customer c
 JOIN address a ON a.address_id = c.address_id
 JOIN payment p ON p.customer_id = c.customer_id
 JOIN city ci ON ci.city_id = a.city_id
 JOIN country co ON co.country_id = ci.country_id
-WHERE co.country LIKE '%ee%'
+WHERE co.country LIKE '%Greece%' AND c.first_name Like 'Linda';
+
+/*Third query List of most rented film in descending orders with */
+SELECT f.title, COUNT(r.rental_id) AS Rental_count, SUM(p.amount) AS Total_amount , AVG(p.amount) AS Avg_amount_per_rental,
+STDDEV(p.amount) AS Standard_deviation FROM film f
+JOIN film fi ON fi.film_id = f.film_id
+JOIN inventory i ON i.film_id  = f.film_id 
+JOIN rental r ON r.inventory_id = i.inventory_id
+JOIN customer c ON c.customer_id = r.customer_id
+JOIN payment p ON p.customer_id = c.customer_id
+GROUP BY f.title
+ORDER BY  SUM(p.amount) desc ;
+
+/*Country vise revenue*/
+SELECT co.country, COUNT(c.customer_id) as No_of_Customer, SUM (p.amount) as Revenue_from_country FROM Customer c
+JOIN address a ON a.address_id = c.address_id
+JOIN city ci ON ci.city_id = a.city_id
+JOIN country co ON co.country_id = ci.country_id
+JOIN payment p ON p.customer_id = c.customer_id
+GROUP BY co.country
+
+/**/
 
 
 
-SELECT l.language_id, l.name AS language_name, f.film_id, f.title
-FROM language l
-RIGHT OUTER JOIN film f ON l.language_id = f.language_id;
 
 
 
-SELECT
-  r.rental_id,MAX(CASE WHEN a.actor_id = 1 THEN a.first_name END) AS actor1_first_name
-  FROM
-  actor a
-  JOIN film_actor f ON f.actor_id = a.actor_id
-  JOIN inventory i ON i.film_id = f.film_id
-  JOIN rental r ON r.inventory_id = i.inventory_id
-GROUP BY
-  r.rental_id;
+ 
+
+
+
+
+
+
+
   
